1b70ca9fb6c32d786ae1a4ae59b31448d44683bb
joso <joso@joso-PC>
2016-10-17 11:38:10 +0200

    Ejercicio 2 solucionado

--- Ejercicio2/src/TestVolatileThread.java
+++ Ejercicio2/src/TestVolatileThread.java
@@ -1,9 +1,11 @@
 
 public class TestVolatileThread extends Thread {
 	boolean keepRunning = true;
+	//añado volatile a la variable count
+	volatile long count=0;
 
     public void run() {
-        long count=0;
+    //    long count=0;
         while (keepRunning) {
             count++;
         }
