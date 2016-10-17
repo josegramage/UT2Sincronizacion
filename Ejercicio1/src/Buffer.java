f22d02e7b9a725c10b3518c6a11babb2020e1ccb
joso <joso@joso-PC>
2016-10-17 11:36:13 +0200

    Ejercicio 1 solucionado

--- Ejercicio1/src/Buffer.java
+++ Ejercicio1/src/Buffer.java
@@ -8,20 +8,26 @@ class Buffer {
       buffer = new char[size];
    }
  
-   public synchronized void put(char c) {
-     while(count == buffer.length) ;
+   public synchronized void put(char c) throws InterruptedException {
+	   //añado wait()
+     while(count == buffer.length)wait() ;
      System.out.println("Produciendo " + c + " ...");
      buffer[in] = c; 
      in = (in + 1) % buffer.length; 
      count++;
+     //añado notifyAll()
+     notifyAll();
    }
     
-   public synchronized char get() {
-     while (count == 0) ;
+   public synchronized char get() throws InterruptedException {
+	   //añado wait()
+     while (count == 0)wait() ;
      char c = buffer[out]; 
      out = (out + 1) % buffer.length;
      count--;
      System.out.println("Consumiendo "+ c + " ...");
+     //añado notifyAll()
+     notifyAll();
      return c;
    }
 }
